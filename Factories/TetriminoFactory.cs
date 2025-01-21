8acbfe76b5cf592f093472b63590634e848ddd92
Furkan Bilal Yiğit <bilalyigit594@gmail.com>
2025-01-21 17:26:19 +0300

    [ADD]: Tetrimino factory added

---
+++ Factories/TetriminoFactory.cs
@@ -0,0 +1,221 @@
+﻿using Microsoft.Xna.Framework;
+using TetrisTutorial.Assets.Tetrimino;
+using TetrisTutorial.Enums;
+
+namespace TetrisTutorial.Factories
+{
+    internal class TetriminoFactory
+    {
+        public TetriminoFactory()
+        {
+            
+        }
+
+        public Tetrimino Generate(Tetriminoes shapeType)
+        {
+            Shape[] shapes = new Shape[4];
+            Color color = Color.White;
+
+            switch (shapeType)
+            {
+                case Tetriminoes.I:
+                    {
+                        color = Color.Cyan;
+                        shapes[0] = new Shape(new string[]
+                        {
+                            "0000",
+                            "1111",
+                            "0000",
+                            "0000"
+                        });
+                        shapes[1] = new Shape(new string[]
+                        {
+                            "0010",
+                            "0010",
+                            "0010",
+                            "0010"
+                        });
+                        shapes[2] = new Shape(new string[]
+                        {
+                            "0000",
+                            "0000",
+                            "1111",
+                            "0000"
+                        });
+                        shapes[3] = new Shape(new string[]
+                        {
+                            "0100",
+                            "0100",
+                            "0100",
+                            "0100"
+                        });
+
+                        break;
+                    }
+                case Tetriminoes.O:
+                    {
+                        color = Color.Yellow;
+                        shapes = new Shape[1];
+                        shapes[0] = new Shape(new string[]
+                        {
+                            "11",
+                            "11",
+                        });
+                        break;
+                    }
+                case Tetriminoes.T:
+                    {
+                        color = Color.DeepPink;
+                        shapes[0] = new Shape(new string[]
+                        {
+                            "010",
+                            "111",
+                            "000"
+                        });
+                        shapes[1] = new Shape(new string[]
+                        {
+                            "010",
+                            "011",
+                            "010"
+                        });
+                        shapes[2] = new Shape(new string[]
+                        {
+                            "000",
+                            "111",
+                            "010"
+                        });
+                        shapes[3] = new Shape(new string[]
+                        {
+                            "010",
+                            "110",
+                            "010"
+                        });
+                        break;
+                    }
+                case Tetriminoes.J:
+                    {
+                        color = Color.Blue;
+                        shapes[0] = new Shape(new string[]
+                        {
+                            "100",
+                            "111",
+                            "000"
+                        });
+                        shapes[1] = new Shape(new string[]
+                        {
+                            "011",
+                            "010",
+                            "010"
+                        });
+                        shapes[2] = new Shape(new string[]
+                        {
+                            "000",
+                            "111",
+                            "001"
+                        });
+                        shapes[3] = new Shape(new string[]
+                        {
+                            "010",
+                            "010",
+                            "110"
+                        });
+                        break;
+                    }
+                case Tetriminoes.L:
+                    {
+                        color = Color.Orange;
+                        shapes[0] = new Shape(new string[]
+                        {
+                            "001",
+                            "111",
+                            "000"
+                        });
+                        shapes[1] = new Shape(new string[]
+                        {
+                            "010",
+                            "010",
+                            "011"
+                        });
+                        shapes[2] = new Shape(new string[]
+                        {
+                            "000",
+                            "111",
+                            "100"
+                        });
+                        shapes[3] = new Shape(new string[]
+                        {
+                            "110",
+                            "010",
+                            "010"
+                        });
+
+                        break;
+                    }
+                case Tetriminoes.S:
+                    {
+                        color = Color.Green;
+                        shapes[0] = new Shape(new string[]
+                        {
+                            "011",
+                            "110",
+                            "000"
+                        });
+                        shapes[1] = new Shape(new string[]
+                        {
+                            "010",
+                            "011",
+                            "001"
+                        });
+                        shapes[2] = new Shape(new string[]
+                        {
+                            "000",
+                            "011",
+                            "110"
+                        });
+                        shapes[3] = new Shape(new string[]
+                        {
+                            "100",
+                            "110",
+                            "010"
+                        });
+
+                        break;
+                    }
+                case Tetriminoes.Z:
+                    {
+                        color = Color.Red;
+                        shapes[0] = new Shape(new string[]
+                        {
+                            "110",
+                            "011",
+                            "000"
+                        });
+                        shapes[1] = new Shape(new string[]
+                        {
+                            "001",
+                            "011",
+                            "010"
+                        });
+                        shapes[2] = new Shape(new string[]
+                        {
+                            "000",
+                            "110",
+                            "011"
+                        });
+                        shapes[3] = new Shape(new string[]
+                        {
+                            "010",
+                            "110",
+                            "100"
+                        });
+
+                        break;
+                    }
+            }
+            Tetrimino tetrimino = new();
+            tetrimino.SetShape(shapes, shapeType, color);
+
+            return tetrimino;
+        }
+    }
+}
