853f547cb3d8c41e35826ce304dba68719e04a86
Furkan Bilal Yiğit <bilalyigit594@gmail.com>
2025-01-21 15:15:44 +0300

    Add project files.

---
+++ Scenes/SceneManager.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace TetrisTutorial.Scenes
+{
+    public class SceneManager
+    {
+        private Stack<IScene> _scenes;
+
+        public SceneManager()
+        {
+            _scenes = new Stack<IScene>();
+        }
+
+        public void PushScene(IScene scene) => _scenes.Push(scene);
+
+        public void PopScene()
+        {
+            if (_scenes.Count > 0)
+                _scenes.Pop();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if(_scenes.Count > 0)
+                _scenes.Peek().Update(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+        {
+            if (_scenes.Count > 0)
+                _scenes.Peek().Draw(spriteBatch, gameTime);
+        }
+    }
+}
