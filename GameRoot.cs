9cb7f5d2c36cd24202f8d66d4f1a9c91218815ad
Furkan Bilal Yiğit <bilalyigit594@gmail.com>
2025-01-21 16:25:21 +0300

    basic red cube

--- GameRoot.cs
+++ GameRoot.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using TetrisTutorial.Assets;
+using TetrisTutorial.Utils;
 
 namespace TetrisTutorial
 {
@@ -9,6 +11,8 @@ namespace TetrisTutorial
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        private Camera _camera;
+        private BasicEffect _shader;
         public GameRoot()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -29,7 +33,6 @@ namespace TetrisTutorial
             _graphics.IsFullScreen = true;
 #endif
             _graphics.ApplyChanges();
-
             base.Initialize();
         }
 
@@ -38,6 +41,22 @@ namespace TetrisTutorial
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            Models.Initialize(Content);
+            _camera = new Camera(new Vector3(0, 0, 5), new Vector3(0, 0, -5), _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight, MathHelper.ToRadians(60));
+
+            #region Shader
+            _shader = new BasicEffect(GraphicsDevice)
+            {
+                View = _camera.View,
+                Projection = _camera.Projection
+            };
+
+            _shader.EnableDefaultLighting();
+            _shader.PreferPerPixelLighting = true;
+            _shader.SpecularPower = 16f;
+            #endregion
+            
+            _spriteBatch.Begin();
         }
 
         protected override void Update(GameTime gameTime)
@@ -52,10 +71,20 @@ namespace TetrisTutorial
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
-            // TODO: Add your drawing code here
-
+            foreach(ModelMesh modelMesh in Models.CubeObject.Meshes)
+            {
+                // This is generic- eventhough the cube only has one meshpart, 
+                // Let's keep the code so you can experiment with different models.
+                foreach (ModelMeshPart modelMeshPart in modelMesh.MeshParts)
+                {
+                    modelMeshPart.Effect = _shader;
+                    _shader.World = Matrix.CreateScale(10) * Matrix.CreateRotationY(0.5f) * Matrix.CreateTranslation(0, 0, -3f);
+                    _shader.DiffuseColor = Color.Red.ToVector3();
+                }
+                modelMesh.Draw();
+            }
             base.Draw(gameTime);
         }
     }
