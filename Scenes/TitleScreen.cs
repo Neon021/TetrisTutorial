f7b1b181ac562d698dd4ad2bc1a03c82a0b06ae4
Furkan Bilal Yiğit <bilalyigit594@gmail.com>
2025-01-21 17:26:52 +0300

    [IMPROV]: tetrimoni draw logic moved to title via factory

--- Scenes/TitleScreen.cs
+++ Scenes/TitleScreen.cs
@@ -1,41 +1,33 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using TetrisTutorial.Assets.Tetrimino;
+using TetrisTutorial.Factories;
 
 namespace TetrisTutorial.Scenes
 {
     internal class TitleScreen : IScene
     {
         private float _angle;
+        private Matrix _world;
+        private Tetrimino _tetrimino;
 
         public TitleScreen()
         {
-
+            TetriminoFactory factory = new();
+            _tetrimino = factory.Generate(Enums.Tetriminoes.J);
         }
 
         public void Update(GameTime gameTime)
         {
             _angle += 0.75f * (float)gameTime.ElapsedGameTime.TotalSeconds;
             _angle %= MathHelper.TwoPi;
+
+            _world = Matrix.CreateScale(5) * Matrix.CreateRotationY(_angle) * Matrix.CreateTranslation(0, 0, -3f);
         }
 
         public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
         {
-
-            // Draw the cube mesh.
-            foreach (ModelMesh m in Assets.Models.CubeObject.Meshes)
-            {
-                // This is generic- eventhough the cube only has one meshpart, 
-                // Let's keep the code so you can experiment with different models.
-                foreach (ModelMeshPart part in m.MeshParts)
-                {
-                    part.Effect = GameRoot.BasicShader;
-
-                    GameRoot.BasicShader.World = Matrix.CreateScale(5) * Matrix.CreateRotationY(_angle) * Matrix.CreateTranslation(0, 0, -3f);
-
-                    GameRoot.BasicShader.DiffuseColor = Color.Red.ToVector3();
-                }
-                m.Draw();
-            }
+            _tetrimino.Draw(_world);
         }
     }
 }
