266195a4ec9405d7664cd7d217e63f4a54738d8e
Furkan Bilal Yiğit <bilalyigit594@gmail.com>
2025-01-21 16:36:25 +0300

    [ADD]: Models class implemented

---
+++ Assets/Models.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TetrisTutorial.Assets
+{
+    internal static class Models
+    {
+        public static Model CubeObject;
+
+        public static void Initialize(ContentManager contentManager)
+        {
+            CubeObject = contentManager.Load<Model>("CubeObject");
+        }
+    }
+}
